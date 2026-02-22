44202873d6a806170a3e04fd88e0e694ea85c8b6
kiteyoung0520 <87756142+kiteyoung0520@users.noreply.github.com>
2026-02-22 23:44:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ppt1/', // 👈 告訴機器人，我們的網頁住在這個子路徑下
 })
